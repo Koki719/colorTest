715c14c877b8e3c3f96621f6a715931054d74979
nagi <uraki777@gmail.com>
2019-06-16 19:33:52 +0900

    create project.

---
+++ colorTest/colorTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  colorTest
+//
+//  Created by 柳澤昂輝 on 2019/06/16.
+//  Copyright © 2019年 柳澤昂輝. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, const char * argv[]) {
+    cv::Mat img;
+    }
