4bca3ac9c95178969e4b84fd312ea7fc33d3ba39
nagi <uraki777@gmail.com>
2019-06-19 17:25:35 +0900

    set color input

--- colorTest/colorTest/main.cpp
+++ colorTest/colorTest/main.cpp
@@ -7,40 +7,63 @@
 //
 
 #include <opencv2/opencv.hpp>
-using namespace std;
-using namespace cv;
+
 int valR = 128;
 void changeRColor(int position, void* userdata); //trackbarコールバック関数（Rの変更）
 int valG = 128;
 void changeGColor(int position, void* userdata); //trackbarコールバック関数（Gの変更）
 int valB = 128;
 void changeBColor(int position, void* userdata); //trackbarコールバック関数（Bの変更）
+
+cv::Mat src(300, 300, CV_8UC3, cv::Scalar::all(127));
+cv::Mat colorTest(1500, 1000, CV_8UC3, cv::Scalar::all(255));
+
 int main()
 {
-    Mat src(128, 384, CV_8UC3, Scalar::all(127));
-    namedWindow("Color Image");
+    cv::namedWindow("Color Image");
     //トラックバーの設置
-    createTrackbar("Red", "Color Image", &valR, 255, changeRColor);
-    createTrackbar("Green", "Color Image", &valG, 255, changeGColor);
-    createTrackbar("Blue", "Color Image", &valB, 255, changeBColor);
-    while (1) {
-        src.setTo(Scalar(valB, valG, valR)); //RGBのすべての値更新
-        imshow("Color Image", src);
-        if(waitKey(30) == 27) break;
-    }
+    cv::createTrackbar("Red", "Color Image", &valR, 255, changeRColor);
+    cv::createTrackbar("Green", "Color Image", &valG, 255, changeGColor);
+    cv::createTrackbar("Blue", "Color Image", &valB, 255, changeBColor);
+    imshow("Color Image", src);
+    
+    cv::putText(colorTest, "Input Color", cv::Point(400,30), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(1,1,1));
+    
+    cv::Mat tmp = colorTest.clone();
+    cv::rectangle(tmp, cv::Point(450,50),cv::Point(550,150), cv::Scalar(valB, valG, valR),-1);
+    
+    cv::putText(tmp, "(" + std::to_string(valR) + ", " + std::to_string(valG)+ ", " + std::to_string(valB) + ")", cv::Point(340,200), cv::FONT_HERSHEY_DUPLEX, 1.2, cv::Scalar(1,1,1));
+    imshow("colorTest", tmp);
+    cv::waitKey(0);
     return 0;
 }
+
+void changedColor(void){
+    src.setTo(cv::Scalar(valB, valG, valR));
+    imshow("Color Image", src);
+    
+    cv::Mat tmp = colorTest.clone();
+    cv::rectangle(tmp, cv::Point(450,50),cv::Point(550,150), cv::Scalar(valB, valG, valR), -1);
+    
+    cv::putText(tmp, "(" + std::to_string(valR) + ", " + std::to_string(valG)+ ", " + std::to_string(valB) + ")", cv::Point(50,200), cv::FONT_HERSHEY_DUPLEX, 1.2, cv::Scalar(1,1,1));
+    imshow("colorTest", tmp);
+}
+
 //trackbarコールバック関数（Rの変更）
 void changeRColor(int position, void* userdata){
     valR = position; //トラックバーの位置によってRの値を更新
+    changedColor();
 }
 // trackbarコールバック関数（Gの変更）
 void changeGColor(int position, void* userdata)
 {
     valG = position; //トラックバーの位置によってGの値を更新
+    changedColor();
 }
 // trackbarコールバック関数（Bの変更）
 void changeBColor(int position, void* userdata)
 {
     valB = position; //トラックバーの位置によってBの値を更新
+    changedColor();
 }
+
