f79756d60e0ff632a1bc28530596405fc23925b5
nagi <uraki777@gmail.com>
2019-06-20 15:49:14 +0900

    complete

--- colorTest/colorTest/main.cpp
+++ colorTest/colorTest/main.cpp
@@ -9,6 +9,7 @@
 #include <opencv2/opencv.hpp>
 
 int numofcolors = 24;
+std::vector<cv::Scalar> colorset{cv::Scalar(68,82,115),cv::Scalar(130,150,194),cv::Scalar(157,122,98),cv::Scalar(67,108,87),cv::Scalar(177,128,133),cv::Scalar(170,189,103),cv::Scalar(44,126,214),cv::Scalar(166,91,80),cv::Scalar(99,90,193),cv::Scalar(108,60,94),cv::Scalar(64,188,157),cv::Scalar(46,163,224),cv::Scalar(150,61,56),cv::Scalar(73,148,70),cv::Scalar(60,54,175),cv::Scalar(31,199,231),cv::Scalar(149,86,187),cv::Scalar(161,133,8),cv::Scalar(242,243,243),cv::Scalar(200,200,200),cv::Scalar(160,160,160),cv::Scalar(121,122,122),cv::Scalar(85,85,85),cv::Scalar(52,52,52)};
 
 int valR = 128;
 void changeRColor(int position, void* userdata); //trackbarコールバック関数（Rの変更）
@@ -23,31 +24,34 @@ cv::Mat projector(1920,1080, CV_8UC3);
 
 int main()
 {
+    std::cout << colorset[0] + colorset[1];
     cv::namedWindow("Color Image");
     cv::namedWindow("projector");
     //トラックバーの設置
     cv::createTrackbar("Red", "Color Image", &valR, 255, changeRColor);
     cv::createTrackbar("Green", "Color Image", &valG, 255, changeGColor);
     cv::createTrackbar("Blue", "Color Image", &valB, 255, changeBColor);
-    imshow("Color Image", src);
+    cv::imshow("Color Image", src);
     
     cv::putText(colorTest, "Input Color", cv::Point(400,30), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(1,1,1));
     for(int i = 0; i < numofcolors; i++){
-        cv::rectangle(colorTest, cv::Point(100 + (i / 12) * 450, 250 + (i % 12) * 75), cv::Point(150 + (i / 12) * 450, 300 + (i % 12) * 75), cv::Scalar(valB, valG,valR), -1);
+        cv::rectangle(colorTest, cv::Point(100 + (i / 12) * 450, 250 + (i % 12) * 75), cv::Point(150 + (i / 12) * 450, 300 + (i % 12) * 75), colorset[i], -1);
         cv::putText(colorTest, "+", cv::Point(200 + (i / 12) * 450, 285 + (i % 12) * 75), cv::FONT_HERSHEY_DUPLEX, 1.2, cv::Scalar(1,1,1));
-        cv::rectangle(colorTest, cv::Point(270 + (i / 12) * 450, 250 + (i % 12) * 75), cv::Point(320 + (i / 12) * 450, 300 + (i % 12) * 75), cv::Scalar(valB, valG,valR), -1);
         cv::putText(colorTest, "=", cv::Point(370 + (i / 12) * 450, 285 + (i % 12) * 75), cv::FONT_HERSHEY_DUPLEX, 1.2, cv::Scalar(1,1,1));
-        cv::rectangle(colorTest, cv::Point(440 + (i / 12) * 450, 250 + (i % 12) * 75), cv::Point(490 + (i / 12) * 450, 300 + (i % 12) * 75), cv::Scalar(valB, valG,valR), -1);
     }
     
     cv::Mat tmp = colorTest.clone();
     cv::rectangle(tmp, cv::Point(450,50),cv::Point(550,150), cv::Scalar(valB, valG, valR),-1);
     cv::putText(tmp, "(" + std::to_string(valR) + ", " + std::to_string(valG)+ ", " + std::to_string(valB) + ")", cv::Point(340,200), cv::FONT_HERSHEY_DUPLEX, 1.2, cv::Scalar(1,1,1));
-    imshow("colorTest", tmp);
+    for(int i = 0; i < numofcolors; i++){
+        cv::rectangle(tmp, cv::Point(270 + (i / 12) * 450, 250 + (i % 12) * 75), cv::Point(320 + (i / 12) * 450, 300 + (i % 12) * 75), cv::Scalar(valB,valG,valR), -1);
+        cv::rectangle(tmp, cv::Point(440 + (i / 12) * 450, 250 + (i % 12) * 75), cv::Point(490 + (i / 12) * 450, 300 + (i % 12) * 75), cv::Scalar(valB,valG,valR) + colorset[i], -1);
+    }
+    cv::imshow("colorTest", tmp);
     projector.setTo(cv::Scalar(valB, valG, valR));
     cv::moveWindow("projector", 2560, 0);
     cv::setWindowProperty("projector", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
-    imshow("projector", projector);
+    cv::imshow("projector", projector);
     cv::waitKey(0);
     return 0;
 }
@@ -55,21 +59,25 @@ int main()
 void changedColor(void){
     src.setTo(cv::Scalar(valB, valG, valR));
     projector.setTo(cv::Scalar(valB, valG, valR));
-    imshow("Color Image", src);
-    imshow("projector", projector);
+    cv::imshow("Color Image", src);
+    cv::imshow("projector", projector);
     cv::Mat tmp = colorTest.clone();
     cv::rectangle(tmp, cv::Point(450,50),cv::Point(550,150), cv::Scalar(valB, valG, valR), -1);
-    
-    cv::putText(tmp, "(" + std::to_string(valR) + ", " + std::to_string(valG)+ ", " + std::to_string(valB) + ")", cv::Point(50,200), cv::FONT_HERSHEY_DUPLEX, 1.2, cv::Scalar(1,1,1));
-    imshow("colorTest", tmp);
+    cv::putText(tmp, "(" + std::to_string(valR) + ", " + std::to_string(valG)+ ", " + std::to_string(valB) + ")", cv::Point(340,200), cv::FONT_HERSHEY_DUPLEX, 1.2, cv::Scalar(1,1,1));
+    for(int i = 0; i < numofcolors; i++){
+        cv::rectangle(tmp, cv::Point(270 + (i / 12) * 450, 250 + (i % 12) * 75), cv::Point(320 + (i / 12) * 450, 300 + (i % 12) * 75), cv::Scalar(valB,valG,valR), -1);
+        cv::rectangle(tmp, cv::Point(440 + (i / 12) * 450, 250 + (i % 12) * 75), cv::Point(490 + (i / 12) * 450, 300 + (i % 12) * 75), cv::Scalar(valB,valG,valR) + colorset[i], -1);
+    }
+    cv::imshow("colorTest", tmp);
 }
 
 void dispColor(void){
     cv::Mat tmp = colorTest.clone();
     for(int i = 0; i < numofcolors; i++){
-        cv::rectangle(colorTest, cv::Point(270 + (i / 12) * 450, 250 + (i % 12) * 75), cv::Point(320 + (i / 12) * 450, 300 + (i % 12) * 75), cv::Scalar(valB, valG,valR), -1);
-        cv::rectangle(colorTest, cv::Point(440 + (i / 12) * 450, 250 + (i % 12) * 75), cv::Point(490 + (i / 12) * 450, 300 + (i % 12) * 75), cv::Scalar(valB, valG,valR), -1);
+        cv::rectangle(tmp, cv::Point(270 + (i / 12) * 450, 250 + (i % 12) * 75), cv::Point(320 + (i / 12) * 450, 300 + (i % 12) * 75), cv::Scalar(valB, valG,valR), -1);
+        cv::rectangle(tmp, cv::Point(440 + (i / 12) * 450, 250 + (i % 12) * 75), cv::Point(490 + (i / 12) * 450, 300 + (i % 12) * 75), cv::Scalar(valB, valG,valR), -1);
     }
+    cv::imshow("colorTest", tmp);
 }
 
 //trackbarコールバック関数（Rの変更）
